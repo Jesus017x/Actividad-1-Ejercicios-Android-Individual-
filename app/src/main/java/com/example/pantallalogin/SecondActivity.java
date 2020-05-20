6ac62753fd026616881834bdeefe19ade74054cb
AjMaster <52114647+Jesus017x@users.noreply.github.com>
2020-05-20 16:58:52 -0400

    First Commit

---
+++ app/src/main/java/com/example/pantallalogin/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.pantallalogin;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usercorrecto);
+
+    }
+
+}
